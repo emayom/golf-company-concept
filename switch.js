bd3f60675e1329612e4c981f83e72334f0fdbb43
emayom <emayom@naver.com>
2021-08-06 00:04:40 +0900

    golf-company-concept #switch-mode

---
+++ switch.js
@@ -0,0 +1,26 @@
+const checkbox = document.getElementById("chk-box");
+const logoImg = document.getElementById("logo-img");
+const navs = document.querySelectorAll(".nav");
+
+if(checkbox) {
+    checkbox.addEventListener("click", handleMode);
+}
+
+function handleMode(){
+
+    if(document.body.className === "light-mode") {
+        document.body.classList.remove("light-mode");
+        document.body.classList.add("dark-mode");
+        logoImg.src = "/img/Logo-dark.png";
+
+        navs.forEach((el)=> el.classList.add("dark-mode"));
+        
+        
+    } else if (document.body.className === "dark-mode") {
+        document.body.classList.add("light-mode");
+        document.body.classList.remove("dark-mode");
+        logoImg.src = "/img/Logo-white.png";
+
+        navs.forEach((el)=> el.classList.remove("dark-mode"));
+    }
+}
